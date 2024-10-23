b6656e84041df5f01aa412ab467ff7de79f7671e
Larisa <laurabrovkina@gmail.com>
2024-10-23 16:36:45 +1300

    Add Identity and Weather project

--- DemoApi/Program.cs
+++ DemoApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,7 +7,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey("ForTheLoveOfGodStoreAndLoadThisSecurely"u8.ToArray()),
+            ValidIssuer = "https://id.dometrain.com",
+            ValidAudience = "https://dometrain.com",
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ValidateIssuer = true,
+            ValidateAudience = true
+        };
+    });
+
 var app = builder.Build();
+
+app.UseAuthorization();
+app.UseAuthentication();
 
 if (app.Environment.IsDevelopment())
 {
@@ -17,13 +38,12 @@ if (app.Environment.IsDevelopment())
             .WithTheme(ScalarTheme.Purple)
             .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient)
             .WithDarkModeToggle(true)
-            .WithApiKeyAuthentication(keyOptions => keyOptions.Token = "apikey");
+            .WithPreferredScheme("ApiKey")
+            .WithApiKeyAuthentication(x => x.Token = "my-api-key");
     });
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
